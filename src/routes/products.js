79ed0fcdfc7df9041f8df4fc68f82b4acb6cab32
unknown <valen1984@gmail.com>
2022-10-03 22:45:31 -0300

    MAIN: Agrego endpoints get,post,put,delete

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,8 @@ const productController = require('../controllers/products');
 const express = require('express');
 const router = express.Router();
 
+//Agrego endpoints get,post,put,delete
+
 router.get('/products', productController.getAllProducts);
 router.get('/products/:id', productController.getProductByIdOrName);
 router.post('/products', productController.createProduct);
